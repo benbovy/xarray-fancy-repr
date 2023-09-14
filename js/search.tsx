a90f7e4b2fa7b2cf5ea78025e538803fe3fc4ec9
Benoit Bovy <benbovy@gmail.com>
2023-09-14 16:03:44 +0200

    search input alternative color when active

--- js/search.tsx
+++ js/search.tsx
@@ -3,15 +3,18 @@ import { useModelState } from "@anywidget/react";
 
 export const Search = () => {
   const [_, setQuery] = useModelState<string>("_filter_query");
+  const [active, setActive] = React.useState<boolean>(false);
 
   let inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     var lowerCase = e.target.value.toLowerCase();
+    setActive(!!lowerCase);
     setQuery(lowerCase);
   };
 
   return (
     <input
       type="text"
+      className={active ? "active" : ""}
       onChange={inputHandler}
       placeholder="Search by dimension..."
     />
