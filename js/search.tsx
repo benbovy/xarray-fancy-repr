2b0198ec7e4471e696f1df716385a65e1e1c280e
Benoit Bovy <benbovy@gmail.com>
2023-09-14 14:22:46 +0200

    re-organize ts(x) modules

---
+++ js/search.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { useModelState } from "@anywidget/react";
+
+export const Search = () => {
+    const [_, setQuery] = useModelState<string>("_filter_query");
+
+    let inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        var lowerCase = e.target.value.toLowerCase();
+        setQuery(lowerCase);
+    };
+
+    return (
+        <input
+            type="text"
+            onChange={inputHandler}
+            placeholder="Search by dimension..."
+        />
+    )
+}
---
+++ js/types.ts
@@ -0,0 +1,24 @@
+export type Dims = string[];
+export type DimInfo = {
+    [key: string]: {"size": number, "hasIndex": boolean}
+}
+
+export type Attrs = {
+    [key: string]: string
+}
+
+export type Index = {
+    coordNames: string[],
+    inlineRepr: string,
+    repr: string
+}
+
+export type Variable = {
+    name: string,
+    hasIndex: boolean,
+    dims: Dims,
+    dtype: string,
+    inlineRepr: string,
+    attrs: Attrs,
+    dataRepr: string
+}
